25e5c21222164bf9c798a6da615edafb43c43593
maybethee <jozalamun@gmail.com>
2024-04-27 19:00:14 -0400

    Fix usernames not being saved correctly, and display them along posts on index

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  before_create :set_username
+
   has_many :posts
+
+  private
+
+  def set_username
+    # auto generate username from first half of email
+    self.username = self.email.split('@').first if self.username.blank?
+  end
 end
